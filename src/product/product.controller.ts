ebcc9462cc50368a62177f9d3016ec323603758e
Fairuz Sakhiy <sakhiyfairuz@gmail.com>
2023-10-31 15:57:24 +0700

    added product services

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -6,28 +6,36 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UsePipes,
+  Request,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { ValidationPipe } from 'src/validation/validation.pipe';
+import { PurchaseProductDto } from './dto/purchase-product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @UseGuards(AuthGuard)
+  @Post('create')
+  @UsePipes(ValidationPipe)
+  create(@Body() createProductDto: CreateProductDto, @Request() req) {
+    return this.productService.create(createProductDto, req.user.uuid);
   }
 
-  @Get()
+  @Get('all')
   findAll() {
     return this.productService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.productService.findOne(+id);
+  @Get('uuid/:uuid')
+  findOne(@Param('uuid') id: string) {
+    return this.productService.findOne(id);
   }
 
   @Patch(':id')
@@ -37,6 +45,13 @@ export class ProductController {
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return this.productService.remove(+id);
+    return this.productService.remove(id);
+  }
+
+  @UseGuards(AuthGuard)
+  @Post('buy')
+  @UsePipes(ValidationPipe)
+  getWALink(@Body() buyProduct: PurchaseProductDto) {
+    return this.productService.generateWALink(buyProduct);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,167 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { Prisma } from '@prisma/client';
+import {
+  FailedResponse,
+  SuccessResponse,
+} from 'src/common/response/response.dto';
+import { Product } from './entities/product.entity';
+import { PurchaseProductDto } from './dto/purchase-product.dto';
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto, userId: string) {
+    const shop = await this.prismaService.seller.findUnique({
+      where: {
+        userId: userId,
+      },
+    });
+
+    if (!shop) {
+      throw new BadRequestException(
+        new FailedResponse("seller doesn't exists", {
+          sellerExists: false,
+          message: 'you need to open up a shop',
+        }),
+      );
+    }
+
+    try {
+      await this.prismaService.product.create({
+        data: {
+          name: createProductDto.name,
+          category: createProductDto.category,
+          price: createProductDto.price,
+          slogan: createProductDto.slogan,
+          description: createProductDto.description,
+          sellerId: shop.uuid,
+        },
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new BadRequestException(
+          new FailedResponse('cannot create data', {
+            code: e.code,
+            message: e.message,
+            meta: e.meta,
+          }),
+        );
+      }
+    }
+
+    return new SuccessResponse('successfully created data');
   }
 
-  findAll() {
-    return `This action returns all product`;
+  async findAll() {
+    const items = await this.prismaService.product.findMany({
+      include: {
+        seller: true,
+      },
+    });
+    const formattedItems: Product[] = [];
+    items.forEach((item) => {
+      const formattedItem: Product = {
+        uuid: item.uuid,
+        name: item.name,
+        category: item.category,
+        description: item.description,
+        photo: item.photo,
+        price: item.price,
+        seller: item.seller.name,
+      };
+      formattedItems.push(formattedItem);
+    });
+    return new SuccessResponse('data retrieved', formattedItems);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: string) {
+    const product = await this.prismaService.product.findFirst({
+      where: {
+        uuid: id,
+      },
+      include: {
+        seller: true,
+      },
+    });
+
+    if (!product) {
+      throw new NotFoundException(
+        new FailedResponse('data not found', { exists: false }),
+      );
+    }
+
+    const formattedProduct: Product = {
+      uuid: product.uuid,
+      name: product.name,
+      category: product.category,
+      description: product.description,
+      photo: product.photo,
+      price: product.price,
+      seller: product.seller.name,
+    };
+
+    return new SuccessResponse('data retrieved', formattedProduct);
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
     return `This action updates a #${id} product`;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: string) {
+    try {
+      await this.prismaService.product.delete({
+        where: {
+          uuid: id,
+        },
+      });
+    } catch (e) {
+      if (e instanceof Prisma.PrismaClientKnownRequestError) {
+        throw new BadRequestException(
+          new FailedResponse('cannot delete data', {
+            code: e.code,
+            message: e.message,
+          }),
+        );
+      }
+
+      throw new BadRequestException(
+        new FailedResponse('cannot delete data', { message: e }),
+      );
+    }
+  }
+
+  async generateWALink(buyProduct: PurchaseProductDto) {
+    const productData = await this.prismaService.product.findFirst({
+      where: {
+        uuid: buyProduct.productId,
+      },
+      include: {
+        seller: {
+          include: {
+            user: true,
+          },
+        },
+      },
+    });
+
+    if (!productData) {
+      throw new NotFoundException(
+        new FailedResponse('product not found', { exists: false }),
+      );
+    }
+
+    const message = `Halo saya mau beli produk ${productData.name} sebanyak ${buyProduct.quantity} buah. Apakah produk ini ready?`;
+    const encodedMessage = encodeURI(message);
+    const link = `https://wa.me/${productData.seller.user.phone}?text=${encodedMessage}`;
+
+    return new SuccessResponse('success', { link: link });
   }
 }
